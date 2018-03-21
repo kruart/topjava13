dae44c38fb4e8e88790d82817ea9c0fe578fc12a
we0z <weoz@ukr.net>
2018-03-21 16:34:44 +0200

    HW03 implement JdbcMealRepositoryImpl

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -11,12 +11,12 @@ INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
 
-INSERT INTO meals (description, calories, user_id) VALUES
-  ('пельмешки', 535, 100000),
-  ('sushi', 407, 100000),
-  ('spaghetti', 321, 100000),
-  ('burger', 378, 100001),
-  ('potatoes', 412, 100001),
-  ('перекус', 216, 100001),
-  ('yogurt', 260, 100001);
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+  ('2018-03-20 11:00', 'пельмешки', 535, 100000),
+  ('2018-03-21 13:00', 'sushi', 407, 100000),
+  ('2018-03-21 20:00', 'spaghetti', 321, 100000),
+  ('2018-03-20 13:30', 'burger', 378, 100001),
+  ('2018-03-21 11:00', 'potatoes', 412, 100001),
+  ('2018-03-21 13:00', 'перекус', 216, 100001),
+  ('2018-03-21 20:00', 'yogurt', 260, 100001);
 
