7bbbf3dafcac73709a72514714326cab7edefd9b
we0z <weoz@ukr.net>
2018-03-21 15:35:58 +0200

    HW03 update sql scripts: create meals table and populate it

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (description, calories, user_id) VALUES
+  ('пельмешки', 535, 100000),
+  ('sushi', 407, 100000),
+  ('spaghetti', 321, 100000),
+  ('burger', 378, 100001),
+  ('potatoes', 412, 100001),
+  ('перекус', 216, 100001),
+  ('yogurt', 260, 100001);
+
